1b3a3cae62aa5fc919df1126425221b081ec628f
Diacius <77890144+Diacius@users.noreply.github.com>
2024-06-18 15:15:38 +0100

    add better constants and a test monochrome image, as well as spooled testing

--- capability tester.py
+++ capability tester.py
@@ -3,19 +3,25 @@ USB_vendor = 0x0483
 USB_product = 0xa19d
 # Newlines to get print above the tear bar - you may want to change this for your printer
 feed_to_bar = '\n\n\n\n'
-from escpos.printer import Usb
-from escpos import constants, codepages, exceptions
+import escpos.printer as printer, escpos.exceptions, escpos.escpos
 import pipsta_constants
 # Setup a temporary connection to check we can connect to the printer
 try:
-    usb = Usb(USB_vendor, USB_product, 0, out_ep=0x2)
+    usb = printer.Usb(USB_vendor, USB_product, 0, out_ep=0x2)
     usb.close()
-except exceptions.DeviceNotFoundError:
+except escpos.exceptions.DeviceNotFoundError:
     print("Printer failed to be found")
 # Setup connection to printer
-usb = Usb(USB_vendor, USB_product, 0, out_ep=0x2)
-# print test graphics data
-usb._raw(pipsta_constants.UNDERLINE + b'\x01Testing' + pipsta_constants.PRINT_AND_FEED + b'\x28')
-
-
-usb.close()
+usb = printer.Usb(USB_vendor, USB_product, 0, out_ep=0x2)
+# Reset formatting
+usb.hw('INIT')
+# Test spooling
+usb._raw(pipsta_constants.ENTER_SPOOLING)
+usb.text('Testing\n')
+usb.text('£€\n') # € prints as ñ, TODO: Add custom codepage so it's corrrect
+# Images currently borked
+#usb.image(img_source='testimg.bmp',impl='bitImageColumn',center=True)
+# 0x32 is DEC: 50 or 2.5 lines (1 line is 20/0x14)
+usb._raw(pipsta_constants.printFeedExtra(b'\x32'))
+# Exit spooling, which should print the full output.
+usb._raw(pipsta_constants.EXIT_SPOOLING)
--- pipsta_constants.py
+++ pipsta_constants.py
@@ -1,11 +1,22 @@
 from escpos.printer import Usb
 from escpos import constants, codepages, exceptions
+from bitarray import bitarray
 ESC = b'\x1B'
+'''
+Initialise
+'''
+INITIALISE = ESC + b'\x40'
+
+
 '''
 Data is stored in buffer until FormFeed or GS,L to exit spooling
 is recieved, or paper feed button is pressed
 '''
 ENTER_SPOOLING = b'\x1B\x4C'
+'''
+Exit Spooling
+'''
+EXIT_SPOOLING = constants.GS + b'L'
 '''
 Underline 1BH,2DH,n:
 if n is 0 then underline is off; otherwise on
@@ -14,4 +25,6 @@ UNDERLINE = b'\x1B\x2D' # Add n byte afterwards
 '''
 Print and feed extra paper, final byte is number of 1/20 lines to feed.
 '''
-PRINT_AND_FEED = b'\x1B\x4A'
+PRINT_AND_FEED_EXTRA_PAPER = b'\x1B\x4A'
+def printFeedExtra(lines:bytes):
+    return PRINT_AND_FEED_EXTRA_PAPER + lines
